2d87faef7a4a41aaf4b1d79ffb113cb8ba9d36ef
Craig <base33@aol.com>
2015-01-19 11:16:49 +0000

    Adding newtonsoft json serialiser

---
+++ WebBlocks/WebBlocks/Controllers/WebBlocksApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+using WebBlocks.Models.Navigation;
+
+namespace WebBlocks.Controllers
+{
+    [PluginController("WebBlocks")]
+    public class WebBlocksApiController : UmbracoApiController//UmbracoAuthorizedApiController
+    {
+        /// <summary>
+        /// Url: /umbraco/WebBlocks/WebBlocksApi/GetChildren?id={id} sample: 1052
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IEnumerable<NavigationItem> GetChildren(int id)
+        {
+            var contentService = UmbracoContext.Application.Services.ContentService;
+
+            IContent root = contentService.GetById(id);
+            
+            return root.Children()
+                .Select(c => new NavigationItem { Id = c.Id, Name = c.Name, ContentType = c.ContentType.Alias, IconClass = c.ContentType.Icon, HasChildren = c.Children().Any() });
+        }
+    }
+}
---
+++ WebBlocks/WebBlocks/Models/Navigation/NavigationItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBlocks.Models.Navigation
+{
+    public class NavigationItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentType { get; set; }
+        public string IconClass { get; set; }
+        public bool HasChildren { get; set; }
+    }
+}
