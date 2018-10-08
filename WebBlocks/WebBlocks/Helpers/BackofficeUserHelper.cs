2f38599f64321f1f1dd276a913253fdec1e60d35
tom <tom.weston@mentordigital.co.uk>
2018-10-08 15:18:36 +0100

    changes to BackofficeUserHelper, simplifies to just IsUserAuthenticated

--- WebBlocks/WebBlocks/Helpers/BackofficeUserHelper.cs
+++ WebBlocks/WebBlocks/Helpers/BackofficeUserHelper.cs
@@ -13,55 +13,17 @@ namespace WebBlocks.Helpers
 	/// </summary>
 	public static class BackofficeUserHelper
 	{
-		const string BackofficeIdentityKey = "UmbracoBackofficeIdentity";
-
-		/// <summary>
-		/// The <c>UmbracoBackOfficeIdentity</c> representing the currently logged on Back Office user
-		/// </summary>
-		public static UmbracoBackOfficeIdentity CurrentIdentity
-		{
-			get
-			{
-				if (!HttpContext.Current.Items.Contains(BackofficeIdentityKey))
-					HttpContext.Current.Items[BackofficeIdentityKey] = new HttpContextWrapper(HttpContext.Current).GetCurrentIdentity(true);
-
-				return (UmbracoBackOfficeIdentity)HttpContext.Current.Items[BackofficeIdentityKey];
-			}
-		}
-
-		/// <summary>
-		/// Determines if there is currently a back office user logged on.
-		/// </summary>
-		public static bool IsUserAuthenticated
-		{
-			get
-			{
-				return CurrentIdentity.IsAuthenticated;
-			}
-		}
-
-		/// <summary>
-		/// Get the Currently logged on backoffice user as old <c>umbraco.BusinessLogic.User</c>
-		/// </summary>
-		/// <returns><c>umbraco.BusinessLogic.User</c> if a user is logged on, otherwise null</returns>
-		public static umbraco.BusinessLogic.User GetCurrentUser()
-		{
-			if (!IsUserAuthenticated)
-				return null;
-
-			return new umbraco.BusinessLogic.User(CurrentIdentity.Name);
-		}
-
-		/// <summary>
-		/// Get the currently logged on backoffice user as new <c>IUser</c>
-		/// </summary>
-		/// <returns><c>IUser</c> if a user is logged on, otherwise null</returns>
-		public static IUser GetCurrentIUser()
-		{
-			if (!IsUserAuthenticated)
-				return null;
-
-			return ApplicationContext.Current.Services.UserService.GetByUsername(CurrentIdentity.Name);
-		}
-	}
+        /// <summary>
+        /// Determines if there is currently a back office user logged on.
+        /// </summary>
+        public static bool IsUserAuthenticated
+        {
+            get
+            {
+                var http = new HttpContextWrapper(HttpContext.Current);
+                var ticket = http.GetUmbracoAuthTicket();
+                return http.AuthenticateCurrentRequest(ticket, false);
+            }
+        }
+    }
 }
